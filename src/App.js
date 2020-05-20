ebc011755b01577954cdae1ff66def92101830bd
shelooks16 <blauyedz@zoho.eu>
2020-05-20 21:19:57 +0300

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+
+      <Route>This is 404 page</Route>
+    </Switch>
+  );
 }
 
 export default App;
