d76c5e19623e5919e61bca53bf7cdea370ec2b50
shelooks16 <blauyedz@zoho.eu>
2020-05-21 18:49:45 +0300

    added layout managment and created title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <div>this is home</div>;
+  return <MainPageLayout>this is home</MainPageLayout>;
 };
 
 export default Home;
--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Starred = () => {
-  return <div>this is starred</div>;
+  return <MainPageLayout>this is starred</MainPageLayout>;
 };
 
 export default Starred;
