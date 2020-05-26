bc81a70529647f4679a662ce4e18ed722df2cf79
shelooks16 <blauyedz@zoho.eu>
2020-05-27 00:56:16 +0300

    defined theme fro styled components and created a few styled components

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+import { NavList, LinkStyled } from './Navs.styled';
 
 const LINKS = [
   { to: '/', text: 'Home' },
@@ -7,15 +9,22 @@ const LINKS = [
 ];
 
 const Navs = () => {
+  const location = useLocation();
+
   return (
     <div>
-      <ul>
+      <NavList>
         {LINKS.map(item => (
           <li key={item.to}>
-            <Link to={item.to}>{item.text}</Link>
+            <LinkStyled
+              to={item.to}
+              className={item.to === location.pathname ? 'active' : ''}
+            >
+              {item.text}
+            </LinkStyled>
           </li>
         ))}
-      </ul>
+      </NavList>
     </div>
   );
 };
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { TitleWrapper } from './Title.styled';
 
 const Title = ({ title, subtitle }) => {
   return (
-    <div>
+    <TitleWrapper>
       <h1>{title}</h1>
       <p>{subtitle}</p>
-    </div>
+    </TitleWrapper>
   );
 };
 
