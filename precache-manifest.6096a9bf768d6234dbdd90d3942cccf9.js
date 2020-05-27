be717036ac0a7a46de3e32fa2396ecfe0c11a054
shelooks16 <blauyedz@zoho.eu>
2020-05-28 00:19:30 +0300

    Updates

--- precache-manifest.6096a9bf768d6234dbdd90d3942cccf9.js
+++ precache-manifest.6096a9bf768d6234dbdd90d3942cccf9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6494ff89d63b2c8be11d0f49a3737ac5",
+    "revision": "8f352b263235bb2860a6cf6ef9b4f705",
     "url": "/box-office/index.html"
   },
   {
-    "revision": "2540076e11032c19a71b",
+    "revision": "a31520027ec9884fe48e",
     "url": "/box-office/static/css/main.93052401.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/box-office/static/js/2.5bbfd9d7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2540076e11032c19a71b",
-    "url": "/box-office/static/js/main.aac00d9d.chunk.js"
+    "revision": "a31520027ec9884fe48e",
+    "url": "/box-office/static/js/main.76465261.chunk.js"
   },
   {
     "revision": "c850b6c555a3d841251e",
