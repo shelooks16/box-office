6577408f296766385273792d426f291b6b7c5a8f
shelooks16 <blauyedz@zoho.eu>
2021-08-28 20:10:21 +0300

    lift-up memoized onStarClick to fix 'rendering more hooks during the previous render'

--- src/components/show/ShowCard.js
+++ src/components/show/ShowCard.js
@@ -21,7 +21,7 @@ const ShowCard = ({ id, image, name, summary, onStarClick, isStarred }) => {
 
       <div className="btns">
         <Link to={`/show/${id}`}>Read more</Link>
-        <button type="button" onClick={onStarClick}>
+        <button type="button" onClick={() => onStarClick(id, isStarred)}>
           <Star active={isStarred} />
         </button>
       </div>
--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -10,19 +10,20 @@ import { useShows } from '../../misc/custom-hooks';
 const ShowGrid = ({ data }) => {
   const [starredShows, dispatchStarred] = useShows();
 
+  const onStarClick = useCallback(
+    (showId, isStarred) => {
+      if (isStarred) {
+        dispatchStarred({ type: 'REMOVE', showId });
+      } else {
+        dispatchStarred({ type: 'ADD', showId });
+      }
+    },
+    [dispatchStarred]
+  );
+
   return (
     <FlexGrid>
       {data.map(({ show }) => {
-        const isStarred = starredShows.includes(show.id);
-
-        const onStarClick = useCallback(() => {
-          if (isStarred) {
-            dispatchStarred({ type: 'REMOVE', showId: show.id });
-          } else {
-            dispatchStarred({ type: 'ADD', showId: show.id });
-          }
-        }, [isStarred, show.id]);
-
         return (
           <ShowCard
             key={show.id}
@@ -31,7 +32,7 @@ const ShowGrid = ({ data }) => {
             image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
             summary={show.summary}
             onStarClick={onStarClick}
-            isStarred={isStarred}
+            isStarred={starredShows.includes(show.id)}
           />
         );
       })}
